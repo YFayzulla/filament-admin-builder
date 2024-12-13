e68c02ae03530773fee79b65423fa79da124ac84
YFayzulla <yoldashevfayzulla0@gmail.com>
2024-12-13 20:40:45 +0500

    my app foulder

---
+++ myapp/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
+
+# Create
+def book_create(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        author = request.POST.get("author")
+        published_date = request.POST.get("published_date")
+        Book.objects.create(title=title, author=author, published_date=published_date)
+        return redirect("book_list")
+    return render(request, "book_create.html")
+
+# Read
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "book_list.html", {"books": books})
+
+# Update
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.title = request.POST.get("title")
+        book.author = request.POST.get("author")
+        book.published_date = request.POST.get("published_date")
+        book.save()
+        return redirect("book_list")
+    return render(request, "book_update.html", {"book": book})
+
+# Delete
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.delete()
+        return redirect("book_list")
+    return render(request, "book_delete.html", {"book": book})
